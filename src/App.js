bf1588d91aba3d1dbd10ebc6d0c0e185ea971ab4
rnormando <rodrigo.normando@omnibees.com>
2019-09-16 18:28:22 -0300

    backup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="Container">
+      <div className="neptune">
+        <div className="neptune-planet"></div>
+        <div className="uranus">
+          <div className="saturn">
+            <div className="jupiter">
+              <div className="mars">
+                <div className="earth">
+                  <div className="venus">
+                    <div className="mercury"></div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="sun"></div>
     </div>
   );
 }
