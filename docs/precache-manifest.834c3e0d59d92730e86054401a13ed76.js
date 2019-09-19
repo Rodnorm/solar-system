697cd147b052842e236ce58adcd99053b11199ae
rnormando <rodrigo.normando@omnibees.com>
2019-09-19 15:11:38 -0300

    deploy

---
+++ docs/precache-manifest.834c3e0d59d92730e86054401a13ed76.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71271e01fd368b8093e10745a63b1895",
+    "url": "/solar-system/index.html"
+  },
+  {
+    "revision": "f2af117e83c712caef57",
+    "url": "/solar-system/static/css/main.6680a112.chunk.css"
+  },
+  {
+    "revision": "806999fa663f346fabd4",
+    "url": "/solar-system/static/js/2.740c4274.chunk.js"
+  },
+  {
+    "revision": "f2af117e83c712caef57",
+    "url": "/solar-system/static/js/main.b5e15524.chunk.js"
+  },
+  {
+    "revision": "2bc89f38376361392d3d",
+    "url": "/solar-system/static/js/runtime~main.b72418e5.js"
+  },
+  {
+    "revision": "9838a502aaaebef9285d0b57151ddb4c",
+    "url": "/solar-system/static/media/earth.9838a502.jpg"
+  },
+  {
+    "revision": "2a38d24d6e58b92410c5f31c7929417c",
+    "url": "/solar-system/static/media/loader.2a38d24d.gif"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a39f90917a1fd2b36de9c8071ad71a24.js"
+  "/solar-system/precache-manifest.834c3e0d59d92730e86054401a13ed76.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/solar-system/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
